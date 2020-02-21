c3fbfa6ce55f31ea3d56c8ea674ce6c0ac786e28
Kalkrin <craigarthurarden@gmail.com>
2020-02-20 21:25:14 -0500

    refactoring

--- MemeScrapper/Form1.cs
+++ MemeScrapper/Form1.cs
@@ -42,20 +42,20 @@ namespace MemeScrapper
             {
                
                 List<Meme> memes = new List<Meme>();
-                string address = GetAppSettings("url");
                 //setup for scraper
                 IConfiguration config = AngleSharp.Configuration.Default.WithDefaultLoader();
                 IBrowsingContext context = BrowsingContext.New(config);
+                IDocument document = await context.OpenAsync(GetAppSettings("url"));
 
                 //Getting the largest page number as memes are paginated
-                int maxPage = await GetMaxPagesFromParentHTML(address, context);
+                int maxPage = document.QuerySelectorAll("div.pagination a").Where(x => !x.ClassList.Contains("next_page")).Select(x => int.Parse(x.InnerHtml)).Max();
 
                 for (int i = 1; i <= 2; i++)
                 {
                     //Looping through each page of memes, getting the URL for each meme and then accessing that url to get data on an individual meme and creating an object for it
-                    address = GetAppSettings("url") + "/page/" + i;
-                    IDocument document = await context.OpenAsync(address);
-                    foreach(IElement element in document.All.Where(x => x.LocalName == "a" && x.GetAttribute("class") == "photo"))
+                    string address = GetAppSettings("url") + "/page/" + i;
+                    IDocument pageDocument = await context.OpenAsync(address);
+                    foreach(IElement element in pageDocument.QuerySelectorAll("td a.photo"))
                      {
                         try
                         {
@@ -65,25 +65,20 @@ namespace MemeScrapper
                             string memeUrl = GetAppSettings("url").Replace(@"/memes", "") + element.GetAttribute("href");
                             IDocument memeDocument = await context.OpenAsync(memeUrl);
 
-                            foreach (IElement ele in memeDocument.All.Where(x => x.LocalName == "a" && x.ParentElement.LocalName == "dd" && x.ParentElement.ParentElement.Id == "entry_tags"))
-                            {
-                                tagsList.Add(ele.InnerHtml);
-                            }
-
                             //Getting the bytearray of the meme image to be stored in the object
                             using (WebClient webClient = new WebClient())
                             {
-                                imageBytes = webClient.DownloadData(memeDocument.QuerySelectorAll("img").FirstOrDefault(x => x.ParentElement.ClassName == "photo left wide"
-                                && x.ParentElement.ParentElement.LocalName == "header").GetAttribute("data-src"));
+                                imageBytes = webClient.DownloadData(memeDocument.QuerySelector("header .photo.left.wide").GetAttribute("href"));
                             }
                             //Creating a meme
                             //This will be replaced by adding the meme to a database instead of just to a list.
                             memes.Add(new Meme
                             {
-                                Name = memeDocument.QuerySelectorAll("a").FirstOrDefault(x => x.ParentElement.ParentElement.ClassName == "info wide").InnerHtml,
+                                //Name = memeDocument.QuerySelectorAll("a").FirstOrDefault(x => x.ParentElement.ParentElement.ClassName == "info wide").InnerHtml,
+                                Name = memeDocument.QuerySelector("section.info.wide h1 a").InnerHtml,
                                 URL = memeUrl,
-                                Views = int.Parse(memeDocument.QuerySelectorAll("a").FirstOrDefault(x => x.ParentElement.ClassName == "views").InnerHtml.Replace(",", "")),
-                                Tags = tagsList.ToArray(),
+                                Views = int.Parse(memeDocument.QuerySelector(".views a").InnerHtml.Replace(",", "")),
+                                Tags = memeDocument.QuerySelectorAll("#entry_tags dd a").Select(x => x.InnerHtml).ToArray(),
                                 ImageByteArray = imageBytes
                             });
                             Console.WriteLine(memes.LastOrDefault().ToString());
@@ -102,25 +97,6 @@ namespace MemeScrapper
                 MessageBox.Show("Please enter a valid path and try again.", "Invalid Path",
                     MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-        }
-
-        public async Task<int> GetMaxPagesFromParentHTML(string address, IBrowsingContext context)
-        {
-            int maxPage = 0;
-
-            IDocument document = await context.OpenAsync(address);
-
-            //Getting each a tag under div of class pagination
-            foreach (IElement element in document.All.Where(x => x.LocalName == "a" && x.ParentElement.ClassList.Contains("pagination")))
-            {
-                int number;
-                //if the innerhtml(pagenumber) is parsable, compare it to maxPage and assign if greater
-                if (int.TryParse(element.InnerHtml, out number))
-                    maxPage = maxPage > number ? maxPage : number;
-            }
-
-            //returning the largest number (last page)
-            return maxPage;
         }
     }
 }
